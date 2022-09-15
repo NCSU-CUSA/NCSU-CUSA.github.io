8cef52f70054771fab0b786582575695a915f7ce
Adrian Chan <amchan@ncsu.edu>
2022-09-15 00:31:55 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: "/home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
